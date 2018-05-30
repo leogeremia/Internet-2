9a8b742b44497965d403403a4a51b7807566edf7
leogeremia <leonelqqs@hotmail.com>
2018-05-30 19:00:26 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+console.log();
+
+var users = [
+    {name: "Gabriel",
+    lastName: "Sattler",
+    age: "19",
+    city: "Rosario",
+    children:["owo","slots","69"]},
+
+    {name: "Tomas",
+    lastName: "Dachesky",
+    age: "19",
+    city: "Rosario",
+    children:["owo","hunt"]},
+
+    {name: "Alexis",
+    lastName: "Bordon",
+    age: "53",
+    city: "Rosario",
+    children:["winner winner","chicken dinner"]},
+
+    {name: "El Inombrable",
+    lastName: "Keilty",
+    age: "??",
+    city: "El Vacio",
+    children:["Satanas", "Dios"]},
+
+    {name: "Pablo",
+    lastName: "G",
+    age: "1",
+    city: "Pochinki",
+    children:["DP"]},
+];
+
+for(var i = 0; i < users.length; i++){
+    console.log(users[i].name + " " + users[i].lastName + " tiene " + users[i].children + " hijos.");
+};
+
+function Sumar(n1, n2){
+    return n1 + n2;
+}
+
+function GetUser(usuario){
+    for(var i = 0; i < users.length; i++){
+        if(users[i].name == usuario){
+            return users[i].lastName + " " + users[i].name + " vive en " + users[i].city;
+        }
+    }
+}
+
+function Usuario(user){
+    return user.lastName + " " + user.name + " vive en " + user.city;
+}
+
+function ListByAge(edad){
+    for(var i = 0; i < users.length; i++){
+        if(users[i].age <= edad){
+            console.log(Usuario(users[i]));
+        }
+    }
+}
+
+console.log(Sumar());
+
+var listusers={ 
+    users:[ ],
+    setusers:function (users) {
+    listusers.users=users;
+}  
+listall: function(users){   
+for (var i=0;i<listusers.users.length;i++){
+console.log(listusers.assamble(users[i]));
+}
+},
+assamble: function(user){
+    return "el usuario " + " " + user.name+ " " + user.lastname + " " + "vive en " + user.city + " " + "y tiene" + user.age + " " + "años" ;
+}, 
+listbyagelimit:function(users,age){
+    for(var i=0;i<listusers.length;i++){
+        if(listusers[i].age<age){
+            console.log(listusers.assamble(listusers[i]));
+        }
+    }
+}
+
+}
